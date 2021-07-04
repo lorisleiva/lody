7f1c3dda92eb2318929f487c73a34de6e9db5c31
Loris Leiva <loris.leiva@gmail.com>
2021-07-04 21:35:52 +0100

    wip

--- tests/ResolveClassnameTest.php
+++ tests/ResolveClassnameTest.php
@@ -6,20 +6,6 @@ use Illuminate\Support\Str;
 use Lorisleiva\Lody\Lody;
 use SplFileInfo;
 
-it('transforms a file into a classname using PSR-4 by default', function () {
-    // Given a file and no custom classname resolver is provided.
-    // -- Note that Testbench Orchestra does not have any class on their
-    // -- test laravel application folder which is why we're using "Models" here
-    // -- instead of "Models/User" for example. But the transformation is the same.
-    $file = new SplFileInfo(base_path('app/Models'));
-
-    // When we resolve the classname of that file.
-    $resolvedPath = Lody::resolveClassname($file);
-
-    // Then we get the classname using the PSR-4 convention.
-    expect($resolvedPath)->toBe('App\Models');
-});
-
 it('uses a custom classname resolver when provided', function () {
     // Given we provide a custom classname resolver
     // that reverses slashes and all segment of a path.
