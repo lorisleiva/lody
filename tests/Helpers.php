3201645ca4bb7fc4c56fced77e731b6d5357dfdb
Loris Leiva <loris.leiva@gmail.com>
2021-07-04 20:56:12 +0100

    wip

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -3,9 +3,10 @@
 namespace Lorisleiva\Lody\Tests;
 
 use Lorisleiva\Lody\FileLazyCollection;
+use Pest\Expectations\Expectation;
 use SplFileInfo;
 
-function expectFilenames(FileLazyCollection $files)
+function expectFilenames(FileLazyCollection $files): Expectation
 {
     $filenames = $files
         ->map(fn (SplFileInfo $file) => $file->getFilename())
